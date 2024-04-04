c9df4d18e8f7acd76a7e28cdb0d72e1b1bd4b684
birgilv <birgittevik84@gmail.com>
2024-04-04 10:59:13 +0200

    Added class for Preemptive Priority scheduling algorithm

---
+++ Assignment 3 CPU-Scheduling/src/main/java/org/example/PPSA.java
@@ -0,0 +1,18 @@
+package org.example;
+
+/**
+ * This is the Preemptive Priority scheduling algorithm
+ */
+public class PPSA {
+    private int processId;
+    private int arrivalTime;
+    private int burstTime;
+    private int priority;
+
+    /**
+     * Creates an instance of the Preemptive Priority scheduling algorithm.
+     */
+    public PPSA(){
+
+    }
+}
