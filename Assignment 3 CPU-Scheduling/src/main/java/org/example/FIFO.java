61a5f1686f006ee7179df1bbc9f30b5c80dd34de
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-04-04 13:40:31 +0200

    ready for javadoc

--- Assignment 3 CPU-Scheduling/src/main/java/org/example/FIFO.java
+++ Assignment 3 CPU-Scheduling/src/main/java/org/example/FIFO.java
@@ -3,7 +3,13 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ */
 public class FIFO {
+  /**
+   * 
+   */
   public static void watingTime(List<Integer> processes, Integer num, List<Integer> burstTime, List<Integer> waitTime) {
     waitTime.add(0, 0);
     for (int i = 1; i < num; i++) {
@@ -11,12 +17,18 @@ public class FIFO {
     }
   }
 
+  /**
+   * 
+   */
   public static void turnAroundTime(List<Integer> processes, Integer num, List<Integer> burstTime, List<Integer> waitTime, List<Integer> tat) {
     for (int i = 0; i < num; i++) {
       tat.add(i, burstTime.get(i) + waitTime.get(i));
     }
   }
 
+  /**
+   * 
+   */
   public static void avgTime(List<Integer> processes, Integer num, List<Integer> burstTime) {
     List<Integer> waitTime = new ArrayList<>(num);
     List<Integer> tat = new ArrayList<>(num);
@@ -26,18 +38,23 @@ public class FIFO {
     watingTime(processes, num, burstTime, waitTime);
     turnAroundTime(processes, num, burstTime, waitTime, tat);
 
-    System.out.println("Processes " + " Burst Time " + " Wati Time " + " Turn AroundTime\n");
+    System.out.println("Processes " + " Burst Time " + " Wait Time " + " Turn AroundTime");
 
     for (int i = 0; i < num; i ++) {
       total_waitTime = total_waitTime + waitTime.get(i);
       total_tat = total_tat + tat.get(i);
-      System.out.println(" " + i+1 + "\t\t" + burstTime.get(i) + "\t " + waitTime.get(i) + "\t\t" + tat.get(i));
+      System.out.println("    " + (i+1) + "\t\t" + burstTime.get(i) + "\t   " + waitTime.get(i) + "\t\t" + tat.get(i));
     }
 
-    System.out.println("Average Waiting Time = " + (double)(total_waitTime / num));
+    System.out.println("Average Wait Time = " + (double)(total_waitTime / num));
     System.out.println("Average Turn Around Time = " + (double)(total_tat / num));
   }
 
+  /**
+   * 
+   *
+   * @param args
+   */
   public static void main(String[] args) {
     List<Integer> processes = new ArrayList<>();
     processes.add(1);
